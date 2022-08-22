f5d1e6fb26b12f663d92eb4f67e1990498557a61
aravindshivakumar <110917376+aravindshivakumar@users.noreply.github.com>
2022-08-21 20:42:16 -0700

    Create Number_guessing_game.py

---
+++ Number guessing game/Number_guessing_game.py
@@ -0,0 +1,33 @@
+import random
+#Snippet code for verifying limit given by user is integer or not.
+
+top_of_range = input("Type a number: ")
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+    if top_of_range <= 0:
+     print("!!! Type a number greater than 0 !!!")
+     quit()
+else:
+    print("!!!Type a valid number!!! ")
+    quit()
+
+#snippet code for user guesses
+random_number = random.randint(0, top_of_range)
+guesses = 0
+while True:
+    guesses += 1
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+     print("!!! Please type a number !!!")
+     continue
+    
+    if user_guess == random_number:
+       print("You got it")
+       break
+    elif user_guess>random_number:
+          print("You were above the number!")
+    else:
+          print("You were below the number!")
+print("You got it in " + str(guesses),"guesses"
